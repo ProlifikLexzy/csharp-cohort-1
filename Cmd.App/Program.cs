ae9fba92dd539b9ff0529d777fa69942748bacb4
Prolifik Lexzy <pro_lifik@live.com>
2021-05-08 11:08:12 +0100

    Add project files.

---
+++ Cmd.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cmd.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Cmd.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cmd.Core
+{
+    public class Class1
+    {
+    }
+}
