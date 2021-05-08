9d2c2d6ba72505949d088323008f581836e81ccd
Prolifik Lexzy <pro_lifik@live.com>
2021-05-08 12:33:36 +0100

    [Proilifik]

---
+++ Cmd.App/Arithmetic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cmd.App
+{
+    //Access Modifier
+    //public, private, internal, protected, protected internal
+    public class ArithmeticCalculator
+    {
+        public static void Add()
+        {
+            Console.WriteLine(1 + 1);
+        }
+        public static void Add(int first)
+        {
+            Console.WriteLine(1 + first);
+        }
+        public static void Add(double first)
+        {
+            Console.WriteLine(1 + first);
+        }
+
+        public static void Add(double first, int second, int third = 9)
+        {
+            Console.WriteLine(first + second + third);
+        }
+    }
+}
--- Cmd.App/Program.cs
+++ Cmd.App/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cmd.App;
+using Cmd.Core;
+using System;
 
 namespace Program
 {
@@ -6,7 +8,24 @@ namespace Program
     {
         public static void Main()
         {
-           
+            
+
+            ArithmeticCalculator.Add(1.9, 1, 7);
+            ArithmeticCalculator.Add(second: 1, first: 1.8, third: 7);
+
+            var lastName = "Bob";
+            var firstName = "Tabor";
+            firstName = lastName;
+            Console.WriteLine("{0} {1}", firstName, lastName);
+            firstName = "Tabor";
+            Console.WriteLine("{0} {1}", firstName, lastName);
+
+            var variable = 4.0999; // implicit typing. literal assignment
+            var v2 = 90;
+
+            Console.WriteLine("{0} {1}", variable, v2);
+            v2 = 1000;
+            Console.WriteLine("{0} {1}", variable, v2);
         }
     }
 }
