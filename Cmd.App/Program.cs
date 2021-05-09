6b79f325292fe0e9123e39264249b6f94bc7b4c6
Prolifik Lexzy <pro_lifik@live.com>
2021-05-09 19:04:58 +0100

    [Prolifik]

--- Cmd.App/Program.cs
+++ Cmd.App/Program.cs
@@ -10,19 +10,26 @@ namespace Program
         tag:
             var input = Console.ReadLine();
 
-            if (input == "f")
-                Console.WriteLine("Is Female");
-            else if (input == "F")
-                Console.WriteLine("Is Female");
-            else if (input == "m")
-                Console.WriteLine("Is Male");
-            else if (input == "M")
-                Console.WriteLine("Is Male");
-            else
-                Console.WriteLine("UnKnown");
+            switch (input)
+            {
+                case "f":
+                case "F":
+                    Console.WriteLine("Is Female");
+                    break;
+                case "m":
+                case "M":
+                    Console.WriteLine("Is Male");
+                    break;
+                case "break":
+                    goto exitTag;
+                default:
+                    Console.WriteLine("UnKnown");
+                    break;
+            }
 
             goto tag;
-            //Console.WriteLine("End of Main");
+            exitTag:
+            Console.WriteLine("End of Main");
         }
 
 
