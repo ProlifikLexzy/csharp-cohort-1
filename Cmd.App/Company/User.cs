e997d600393c2e798c9da2e287edd8979367e18b
Prolifik Lexzy <pro_lifik@live.com>
2021-06-20 19:41:24 +0100

    overload

--- Cmd.App/Company/User.cs
+++ Cmd.App/Company/User.cs
@@ -9,10 +9,35 @@ namespace Cmd.App.Company
     public class User
     {
         public string FullName { get; set; }
+        public int Age { get; set; }
+
+        public static User operator +(User first, User second)
+        {
+            return new User() { Age = first.Age + second.Age };
+        }
+
+        public static bool operator >(User first, User second)
+        {
+            return first.Age > second.Age;
+        }
+
+        public static bool operator <(User first, User second)
+        {
+            return first.Age < second.Age;
+        }
+        public static bool operator ==(User first, User second)
+        {
+            return first.FullName == second.FullName;
+        }
+
+        public static bool operator !=(User first, User second)
+        {
+            return first.FullName != second.FullName;
+        }
 
         public override string ToString()
         {
-            return $"FullName: {FullName}";
+            return $"FullName: {FullName} Age: {this.Age}";
         }
     }
 }
--- Cmd.App/Program.cs
+++ Cmd.App/Program.cs
@@ -21,54 +21,15 @@ namespace Program
         public delegate int MyDelegate(int i);
         public static void Main()
         {
-            //IEnumerable
-            foreach (var item in GetUsers())
-            {
-                Console.WriteLine(item);
-            }
+            var u1 = new User() { FullName = "Segun Ojo", Age = 20 };
+            var u2 = new User() { FullName = "Segun Ojo", Age = 89};
+             var result = --u2;
+
+            
+
+            Console.WriteLine(result);
+
             Console.WriteLine("End of Main");
-        }
-
-        public static IEnumerable<string> GetNames()
-        {
-           
-            //var userArray = new string[]
-            //{
-            //     "Prolifik Lexzy",
-            //    "Scott Hanselman",
-            //    "Scott Hunter",
-            //    "Scott Guthrie"
-            //};
-
-            yield return "Prolifik Lexzy";
-            yield return "Scott Hanselman";
-            yield return "Scott Hunter";
-            yield return "Scott Hunter";
-        }
-
-        public static IEnumerable<User> GetUsers()
-        {
-            var names = new string[]
-            {
-                 "Prolifik Lexzy",
-                "Scott Hanselman",
-                "Scott Hunter",
-                "Scott Guthrie"
-            }; ;
-
-            //foreach (var name in names)
-            //{
-            //    yield return new User() { FullName = name };
-            //}
-
-
-            for (int i = 0; i < names.Length; i++)
-            {
-                yield return new User() { FullName = names[i] };
-            }
-
-            yield return new User() { FullName = "Funke Akindele"};
-
         }
 
     }
