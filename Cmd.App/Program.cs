9b99c1a27dcbb731271079793e92d778654f10fa
Prolifik Lexzy <pro_lifik@live.com>
2021-08-17 03:28:33 +0100

    [Prolifik]

--- Cmd.App/Program.cs
+++ Cmd.App/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using System.Text;
 
 namespace Program
 {
@@ -9,97 +12,138 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                Task.Factory.StartNew(() =>
-                 {
-                     int numberOfSeconds = 0;
-                     while (numberOfSeconds < 10)
-                     {
-                         ++numberOfSeconds;
-                         throw new Exception("ooops");
-                        //Console.WriteLine();
-                        //Thread.Sleep(1000);
-                    }
-
-                     return numberOfSeconds;
-                 }).Wait();
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-
-
-            Task.Run(() =>
-             {
-                 int numberOfSeconds = 50;
-                 while (numberOfSeconds < 70)
-                 {
-                     ++numberOfSeconds;
-                     //Console.WriteLine();
-                     //Thread.Sleep(1000);
-                 }
-
-                 return Convert.ToString(numberOfSeconds);
-             });
-
-
-            Console.WriteLine("Thread Started.");
-
+            UsingMemoryStream();
+            Console.WriteLine("End of Main Method.");
             Console.ReadLine();
         }
 
-        public static List<string> GetCSharpKeywords()
+        private static void UsingMemoryStream()
         {
-            return new List<string>
-            {
-                "abstract","as", "base","bool","break",
-                "byte","case"," catch","  char","checked",
-                "class","const","continue", "decimal","default",
-                "delegate", "do","double"," else","enum",
-                "event","explicit", "extern","false","finally",
-                "fixed", "float","for"," foreach","goto",
-                "if","implicit", "in", "int","interface",
-                "internal", "is","lock","long","namespace",
-                "new","null","object","operator","out",
-                "override", "params", "private", "protected", "public",
-                "readonly ref","return", "sbyte", "sealed",
-                "short","sizeof", "stackalloc",   "static" ,"string",
-                "struct"," switch", "this","throw","true",
-                "try"," typeof", "uint","ulong","unchecked",
-                "unsafe", "ushort", "using"," virtual", "void",
-                "volatile" ,"while"
-            };
+            var memStream = new MemoryStream(100);
+
+            byte[] javaBytes = Encoding.ASCII.GetBytes("Java");
+            var csharpBytes = Encoding.ASCII.GetBytes("CSharp");
+            var v = "Java vs rp";
+            memStream.Write(javaBytes, 0, javaBytes.Length);
+            memStream.Write(csharpBytes, 0, csharpBytes.Length);
+
+            memStream.Seek(-6, SeekOrigin.Current);
+
+            var vsBytes = Encoding.ASCII.GetBytes(" vs ");
+
+            memStream.Write(vsBytes, 0, vsBytes.Length);
+
+            var allBytes = memStream.GetBuffer();
+
+            var message = Encoding.ASCII.GetString(allBytes);
+
+            Console.WriteLine(message);
         }
 
-        public static List<string> GetJavaKeywords()
+        public static void WriteStream()
         {
-            return new List<string>
+            string path = @"c:\base-test\testfile.txt";
+
+            
+            if (File.Exists(path))
             {
-                "abstract", "do", "if", "package", "synchronized",
-                "boolean",  "double", "implements", "private", "this",
-                "break", "else",  "import", "protected",  "throw",
-                "byte", "extends", "instanceof",  " public", "throws",
-                "case", "false", "int", " return", "transient",
-                "catch", "final",   "interface", "short", "true",
-                "char", "finally",    "long", "static",  "try",
-                "class ",  " float", "native",    "strictfp",    " void",
-                "const",  "for",  " new"
-            };
+                File.Delete(path);
+            }
+
+            using (Stream stream = File.Create(path))
+            {
+                using (var streamWriter = new StreamWriter(stream))
+                {
+                    string message = "XOXOXO";
+                    streamWriter.Write(message);
+                }
+            }
+        }
+
+        private static void ReadToStream()
+        {
+            string path = @"c:\base-test\testfile.txt";
+
+            using (Stream stream = new FileStream(path, FileMode.Open))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    var message = streamReader.ReadToEnd();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+
+        private static void ReadFromFile()
+        {
+            string path = @"c:\base-test\testfile.txt";
+            var ascii = new ASCIIEncoding();
+
+            using (Stream stream = new FileStream(path, FileMode.Open))
+            {
+                var bytes = new byte[1000];
+
+                while (stream.Read(bytes, 0, bytes.Length) > 0)
+                {
+                }
+
+                var message = ascii.GetString(bytes);
+                Console.WriteLine(message);
+            }
+        }
+
+        private static void WriteToFile()
+        {
+            string message = "Learning C#";
+            var bytes = GetStringBytes(message);
+            string directory = @"c:\base-test\";
+            string path = @"testfile.txt";
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            string combinePath = directory + path;
+            using (FileStream stream = new FileStream(combinePath, FileMode.Append))
+            {
+                try
+                {
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.WriteByte((byte)'!');
+                    stream.Close();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+
+                }
+            }
+        }
+
+        public static byte[] GetStringBytes(string message)
+        {
+            var bytes = Encoding.ASCII.GetBytes(message);
+            //char[] letters = message.ToCharArray();
+            //byte[] bytes = new byte[letters.Length];
+
+            //for (int i = 0; i < letters.Length; i++)
+            //{
+            //    bytes[i] = (byte)letters[i];
+            //}
+
+            return bytes;
+        }
+
+
+        private static void RunMethod()
+        {
+            Thread.Sleep(5000);
         }
     }
-
-    public class Person
-    {
-        public string FullName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string BloodType { get; set; }
-        public int Age { get; set; } = 100;
-    }
-
 }
 
 
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // var url = @"https://m.media-amazon.com/images/M/MV5BMjhmNDA1M2UtODJlNS00MGNlLTgxZmYtMzE3NzlkMjEzYTYzXkEyXkFqcGdeQXVyMTkyNzM3NDc@._V1_SX300.jpg";
+
+            if (string.IsNullOrWhiteSpace(this.myTextBox.Text))
+                return;
+
+
+            pBar.IsIndeterminate = true;
+
+            var task = GetPhotoAsync();
+
+            var result = task.Result;
+
+            this.photoViewer.Source = LoadImage(result);
+            pBar.IsIndeterminate = false;
+        }
+
+        public async Task<byte[]> GetPhotoAsync()
+        {
+            var client = new HttpClient();
+           await client.SendAsync(null);
+
+            var request = WebRequest.Create(this.myTextBox.Text);
+
+            var buffer = await Task.Run<byte[]>(() =>
+              {
+                  var response = request.GetResponse();
+                  Thread.Sleep(3000);
+                  var stream = response.GetResponseStream();
+                  var streamReader = new BinaryReader(stream);
+                  Byte[] buffer = streamReader.ReadBytes(1 * 1024 * 1024 * 10);
+                  return buffer;
+              });
+
+            return buffer;
+        }
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
